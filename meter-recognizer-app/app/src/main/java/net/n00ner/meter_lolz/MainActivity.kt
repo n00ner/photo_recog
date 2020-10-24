68c1191bd5ff4a33377938df2a2abc564fc9d810
Илья Дубчак <ilia.dubchak@ltlatn.com>
2020-10-24 11:43:56 +0300

    first commit

---
+++ meter-recognizer-app/app/src/main/java/net/n00ner/meter_lolz/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.n00ner.meter_lolz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
